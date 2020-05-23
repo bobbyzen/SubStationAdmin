7c8dca92c171a7b1cc5e1ad7f34e7bc89ad3a6aa
Ricky Leonardo Wijaya <rickylw250100@gmail.com>
2020-05-23 15:23:45 +0700

    t

--- app/src/main/java/com/example/substationadmin/Activity/PengajuanGarduActivity.java
+++ app/src/main/java/com/example/substationadmin/Activity/PengajuanGarduActivity.java
@@ -1,7 +1,10 @@
 package com.example.substationadmin.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
+
+import com.example.substationadmin.R;
 
 public class PengajuanGarduActivity extends AppCompatActivity {
 
